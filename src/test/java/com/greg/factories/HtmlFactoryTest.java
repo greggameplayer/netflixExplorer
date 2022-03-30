8ddd60fbe497f30ebf5e71594617215c2dd957f9
greggameplayer <gregoire.hage@gmail.com>
2022-03-30 10:00:37 +0200

    correct test result

--- src/test/java/com/greg/factories/HtmlFactoryTest.java
+++ src/test/java/com/greg/factories/HtmlFactoryTest.java
@@ -31,6 +31,6 @@ public class HtmlFactoryTest {
     void createMovieListPage() {
         HtmlFactory htmlFactory = new HtmlFactory();
         String result = htmlFactory.createMovieListPage(movies);
-        Assertions.assertEquals("", result);
+        Assertions.assertEquals("<html><head><title>Movie List</title><link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\"><script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js\"></script><link href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css\" rel=\"stylesheet\"></head><body><table class=\"table table-striped table-bordered table-hover table-sm\"><thead><tr><th scope=\"col\">Show id</th><th scope=\"col\">Type</th><th scope=\"col\">Title</th><th scope=\"col\">Director</th><th scope=\"col\">Cast</th><th scope=\"col\">Country</th><th scope=\"col\">Date added</th><th scope=\"col\">More details</th></tr></thead><tbody><tr><th scope=\"row\">s1</th><td>Movie</td><td>The Matrix</td><td>Director</td><td>Cast</td><td>US</td><td>2018</td><td><a href=\"s1.html\" class=\"btn btn-primary btn-sm\">details</a></td></tr><tr><th scope=\"row\">s2</th><td>Movie</td><td>The Matrix: Reloaded</td><td>Director</td><td>Cast</td><td>US</td><td>2018</td><td><a href=\"s2.html\" class=\"btn btn-primary btn-sm\">details</a></td></tr></tbody></table></body></html>", result);
     }
 }
